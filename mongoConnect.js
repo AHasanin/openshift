cd874abb248ae37f1f1122e2549834a84c60d37a
HASANIN, AHMED (AHMED) **CTR** <ahasanin@avaya.com>
2022-06-19 01:43:37 +0200

    initial commit

---
+++ mongoConnect.js
@@ -0,0 +1,18 @@
+const uri = "mongodb://mongo1:27017/?replicaSet=rs0";
+const { MongoClient } = require('mongodb');
+
+const client = new MongoClient(uri);
+module.exports = async () => {
+  try {
+    // Connect the client to the server
+    console.log('connecting')
+    await client.connect();
+    // Establish and verify connection
+    // await client.db("orders");
+    // global.DB = client.db("records");
+    console.log("Connected successfully to server");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
